fddabf6b3088a82d2923ffd8cc27b23e8a1a19b7
Franco Valencia <franco.avalencia@gmail.com>
2024-07-05 18:34:24 -0600

    Implemented reporting to sentry

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	// Setup logs
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	log.Logger = log.Output(output)
+
+	// Setup env
+	viper.AutomaticEnv()
 
 	app := cli.NewApp()
 	app.Name = "probius"
@@ -26,28 +32,51 @@ func main() {
 			Required: true,
 		},
 		&cli.StringFlag{
-			Name:    "influx-url",
-			Aliases: []string{"influx"},
-			Usage:   "Enable sending logs to influx",
+			Name:     "server",
+			Aliases:  []string{"s"},
+			Usage:    "The influx server URL",
+			Required: true,
 		},
 		&cli.StringFlag{
-			Name:    "influx-bucket",
-			Aliases: []string{"bucket"},
-			Usage:   "The bucket that will receive the logs",
+			Name:     "org",
+			Aliases:  []string{"o"},
+			Usage:    "The influx org that owns the bucket",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "bucket",
+			Aliases:  []string{"b"},
+			Usage:    "The influx bucket for the measurements",
+			Required: true,
+		},
+		&cli.IntFlag{
+			Name:        "interval",
+			Aliases:     []string{"i"},
+			Usage:       "The interval in seconds between each probe",
+			DefaultText: "10",
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		probe, err := NewProbe()
+		token := viper.GetString("INFLUX_TOKEN")
+		if token == "" {
+			return cli.Exit(errors.New("Environment variable INFLUX_TOKEN not set"), 1)
+		}
+
+		probe, err := NewProbe(c.String("server"), c.String("org"), c.String("bucket"), token)
 		if err != nil {
 			return cli.Exit(err, 1)
 		}
 
-		err = probe.Run(c.String("destination"))
-		if err != nil {
-			return cli.Exit(err, 1)
+		// Continuous loop to run the probe indefinitely
+		for {
+			err = probe.Run(c.String("destination"))
+			if err != nil {
+				log.Error().Err(err).Msg("Probing error")
+			}
+			// Wait for a short duration before probing again
+			time.Sleep(time.Duration(c.Int("interval")) * time.Second) // Adjust the sleep duration as necessary
 		}
-		return nil
 	}
 
 	err := app.Run(os.Args)
--- probe.go
+++ probe.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
-	"fmt"
 	"os/exec"
+	"strconv"
 	"time"
 
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/rs/zerolog/log"
 	"github.com/vanclief/ez"
 	"github.com/vanclief/network-probe/mtr"
 )
 
-type Probe struct{}
+type Probe struct {
+	InfluxClient influxdb2.Client
+	InfluxOrg    string
+	InfluxBucket string
+}
 
-func NewProbe() (*Probe, error) {
+func NewProbe(influxURL, org, bucket, token string) (*Probe, error) {
 	const op = "NewProbe"
 
-	p := &Probe{}
+	p := &Probe{
+		InfluxOrg:    org,
+		InfluxBucket: bucket,
+		InfluxClient: influxdb2.NewClient(influxURL, token),
+	}
+
+	log.Info().
+		Str("bucket", bucket).
+		Str("org", org).
+		Str("url", influxURL).
+		Msg("Created probe")
 
 	return p, nil
 }
@@ -25,17 +41,32 @@ func NewProbe() (*Probe, error) {
 func (p *Probe) Run(destination string) error {
 	const op = "Probe.Run"
 
+	report, err := p.GenerateReport(destination)
+	if err != nil {
+		return ez.Wrap(op, err)
+	}
+
+	err = p.Send(report)
+	if err != nil {
+		return ez.Wrap(op, err)
+	}
+
+	return nil
+}
+
+func (p *Probe) GenerateReport(destination string) (*mtr.Report, error) {
+	const op = "Probe.GenerateReport"
+
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
-	// TODO: Go back to 10
-	cmd := exec.Command("mtr", "-r", "-c", "1", "--json", destination)
+	cmd := exec.Command("mtr", "-r", "-c", "10", "--json", destination)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
 	err := cmd.Run()
 	if err != nil {
-		return ez.Wrap(op, err)
+		return nil, ez.Wrap(op, err)
 	}
 
 	log.Info().Str("destination", destination).Msg("Ran MTR")
@@ -44,12 +75,53 @@ func (p *Probe) Run(destination string) error {
 
 	err = json.Unmarshal(stdout.Bytes(), &res)
 	if err != nil {
-		return ez.Wrap(op, err)
+		return nil, ez.Wrap(op, err)
 	}
 
 	res.Report.Timestamp = time.Now().Unix()
 
-	fmt.Println("Res", res.Report.String())
+	return &res.Report, nil
+}
+
+func (p *Probe) Send(report *mtr.Report) error {
+	const op = "Probe.Send"
+
+	// Get non-blocking write client
+	writeAPI := p.InfluxClient.WriteAPIBlocking(p.InfluxOrg, p.InfluxBucket)
+
+	// Create a new point using full params constructor
+	for i, hub := range report.Hubs {
+
+		point := influxdb2.NewPoint("probe",
+			map[string]string{"source": report.MTR.Source, "destination": report.MTR.Destination, "host": hub.Host, "hop": strconv.Itoa(i)},
+			map[string]interface{}{
+				"number_of_tests": report.MTR.NumberOfTests,
+				"pattern_size":    report.MTR.PatternSize,
+				"bit_pattern":     report.MTR.BitPattern,
+				"count":           hub.Count,
+				"loss":            hub.LossPercent,
+				"sent":            hub.Sent,
+				"last":            hub.Last,
+				"avg":             hub.Average,
+				"best":            hub.Best,
+				"worst":           hub.Worst,
+				"std":             hub.StandardDeviation,
+			},
+			time.Unix(report.Timestamp, 0))
+
+		err := writeAPI.WritePoint(context.Background(), point)
+		if err != nil {
+			log.Error().Err(err).Msg("Error writing point")
+		}
+	}
+
+	// Write the point
+	err := writeAPI.Flush(context.Background())
+	if err != nil {
+		return ez.Wrap(op, err)
+	}
+
+	log.Info().Str("source", report.MTR.Source).Msg("Written to InfluxDB")
 
 	return nil
 }
